a29cf0194c8fe3171416d6641542b7bccd2ad267
Dhanajeyan Nallasamy <DhanajeyanN@aosta.com>
2019-06-18 15:34:53 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT ||3000;
+
+app.listen(PORT, () => console.log(`Express server currently running on port ${PORT}`));
+
+app.get(`/`, (request, response) => {
+    response.send('Hello, /');
+  });
